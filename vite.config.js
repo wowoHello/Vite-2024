72afe13d72d1842d120d4a9d9d98c589b236e372
wowoHello <pig22630182@gmail.com>
2024-08-07 19:22:56 +0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vite-2024/',
+  base: '/Vite-2024-week1/',
   plugins: [
     vue(),
     vueDevTools(),
